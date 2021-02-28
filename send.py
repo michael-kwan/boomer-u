dbc373135d69b342a3fd2418fc65fb15cd0aff34
Michael Kwan <18michaelkwan@gmail.com>
2021-02-28 05:41:29 -0600

    add airtable

--- send.py
+++ send.py
@@ -3,6 +3,7 @@ import os
 from twilio.rest import Client
 import json
 import random
+import requests
 
 a = open('auth.json')
 auth = json.load(a)
@@ -12,36 +13,46 @@ auth = json.load(a)
 # and set the environment variables. See http://twil.io/secure
 account_sid = auth['TWILIO_ACCOUNT_SID']
 auth_token = auth['TWILIO_AUTH_TOKEN']
+airtable_key = auth['AIRTABLE_KEY']
+airtable_base = auth['AIRTABLE_BASE']
 client = Client(account_sid, auth_token)
 
 
-#get list of numbers
 
-numbers = open("nums.json")
-nums = json.load(numbers)
 
-#get list of things to send
 
-msgs = open("msgs.json")
-messages = json.load(msgs)
+airtable_get = "https://api.airtable.com/v0/{}/{}?api_key={}".format(airtable_base, "Numbers", airtable_key)
+resp = requests.get(airtable_get)
+numbers = json.loads(resp.content.decode("utf-8"))
+num_set = set()
+for row in numbers['records']:
 
-rnum = random.randrange(0, len(messages['words']))
+	num_set.add(row['fields']['Number'])
 
-word = messages['words'][rnum]['word'].upper()
-definition = messages['words'][rnum]['definition']
+
+airtable_get = "https://api.airtable.com/v0/{}/{}?api_key={}".format(airtable_base, "Texts", airtable_key)
+resp = requests.get(airtable_get)
+texts = json.loads(resp.content.decode("utf-8"))
+r_texts = random.randrange(0, len(texts['records']))
+
+term =  texts['records'][r_texts]['fields']['Term']
+definition = texts['records'][r_texts]['fields']['Definition']
+sentence = texts['records'][r_texts]['fields']['Sentence']
 
 msg_body = "Hello from Boomer University! \n\n"\
 "The word of the day is {}, which means: \n\n"\
-"{}".format(word, definition)
+"{} \n\n"\
+"{}".format(term.upper(), definition.capitalize(),sentence)
 
 
-for n in nums['nums']:
-    print(n)
+for n in num_set:
+	print(n)
     message = client.messages \
                     .create(
                          body=msg_body,
                          from_='+12168687855',
-                         to=n['number']
+                         to=n
                     )
 
-print(message.sid)
+# print(message.sid)
+
